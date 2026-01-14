ed49f8cc9ed4c5f18a35c5058f6a4377105c4819
Balandala <balandala05@gmail.com>
2026-01-14 07:42:34 +0500

    layout

---
+++ app/src/main/java/com/example/schedule/MainFragment.kt
@@ -0,0 +1,56 @@
+package com.example.schedule
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
+import com.example.schedule.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var binding: FragmentMainBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.bottomNavigation?.setOnItemSelectedListener {
+            changeTab(it.itemId)
+            true
+        }
+
+    }
+
+    private fun changeTab(@IdRes id: Int) {
+        val navHostId = binding?.navHost?.id
+        navHostId ?: return
+        val transaction = childFragmentManager.beginTransaction()
+        when (id) {
+            //R.id.navigation_today ->
+
+            R.id.navigation_schedule ->
+                transaction.replace(navHostId, ScheduleFragment.newInstance())
+        }
+        transaction.commit()
+    }
+
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            MainFragment().apply {
+
+            }
+    }
+}
